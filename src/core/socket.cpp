a3bf28ef3e6b00b1706e95422f43987a3a63ec56
qinggniq <livewithblank@outlook.com>
2019-03-16 20:31:13 +0800

    finish timer

---
+++ src/core/socket.cpp
@@ -0,0 +1,5 @@
+//
+// Created by wc on 3/14/19.
+//
+
+#include "socket.h"
---
+++ src/core/socket.h
@@ -0,0 +1,10 @@
+//
+// Created by wc on 3/14/19.
+//
+
+#ifndef SRC_SOCKET_H
+#define SRC_SOCKET_H
+namespace qg{
+
+}
+#endif //SRC_SOCKET_H
