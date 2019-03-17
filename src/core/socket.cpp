74d3359f7e142e1b77245c2db923857ee2c5860d
qinggniq <livewithblank@outlook.com>
2019-03-17 15:00:17 +0800

    add Socket class definition

--- src/core/socket.cpp
+++ src/core/socket.cpp
@@ -3,3 +3,25 @@
 //
 
 #include "socket.h"
+#include <sys/socket.h>
+#include <fcntl.h>
+
+namespace qg{
+void Socket::makeAddrReuseable() {
+  qg_int enable = 1;
+  if (setsockopt(sfd_, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(qg_int)) < 0);
+    //TODO (qinggniq) log
+}
+/*
+ * copyright:
+ * author: Jeremy Friesner
+ * https://stackoverflow.com/questions/1543466/how-do-i-change-a-tcp-socket-to-be-non-blocking
+ */
+void Socket::makeNonblock() {
+  qg_int flags = fcntl(sfd_, F_GETFL, 0);
+  if (flags == -1) ;//TODO (qinggniq) log.
+  flags = flags | O_NONBLOCK;
+  if (fcntl(sfd_, F_SETFL, flags) != 0); //TODO (qinggniq) log
+}
+
+}//namespace qg
--- src/core/socket.h
+++ src/core/socket.h
@@ -4,7 +4,32 @@
 
 #ifndef SRC_SOCKET_H
 #define SRC_SOCKET_H
+#include "noncopyable.h"
+#include "type.h"
+/*
+ * 当使用非阻塞的Socket时，如果需要处理已经断开连接的客户端连接，请设置心跳，或者定时任务。
+ *  https://stackoverflow.com/questions/5906175/what-do-a-benefit-from-changing-from-blocking-to-non-blocking-sockets
+ */
 namespace qg{
+class Socket:public noncopyable{
+  Socket(qg_int fd,
+         qg_bool reuse,
+         qg_bool nonblock,
+         qg_bool cork):
+         sfd_(fd),
+         reuse_(reuse),
+         nonblock_(nonblock),
+         cork_(cork){}
 
+  void makeAddrReuseable();
+  void makeNonblock();
+
+ private:
+  qg_int sfd_;
+  qg_bool reuse_;
+  qg_bool nonblock_;
+  qg_bool cork_;
+
+};
 }
 #endif //SRC_SOCKET_H
